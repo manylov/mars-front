59320845dd14d8881b59a3d7fc7f1b8af8485720
Maxim Manylov <manylov@gmail.com>
2025-03-11 21:32:39 +0500

    cleaning

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { OpenGlobus } from '@features/globus/components/openGlobus/OpenGlobus';
 import { PartedMars } from '@features/globus/components/partedMars/PartedMars';
 import { LandsSidebar } from '@features/lands/components/landsSidebar';
 import { useBalance } from '@global/hooks/useBalance';
-import { NETWORK_DATA } from '@root/settings';
 import { CURRENT_CHAIN } from '@root/settings/chains';
 import mixpanel from 'mixpanel-browser';
 
@@ -27,29 +25,15 @@ function MapPage() {
   return (
     <div className="wrapper">
       <LandsSidebar />
-      {NETWORK_DATA.GLOBE === 'arcgis' && (
-        <>
-          <PartedMars
-            allTokens={allMintedTokens}
-            myTokens={tokens}
-            height="100vh"
-            handleClaim={claimToken}
-            balance={userBalance}
-            currency={CURRENT_CHAIN.ticker}
-          />
-        </>
-      )}
-      {NETWORK_DATA.GLOBE === 'openglobus' && (
-        <>
-          <OpenGlobus
-            allTokens={allMintedTokens}
-            myTokens={tokens}
-            height="100vh"
-            balance={userBalance}
-            currency={CURRENT_CHAIN.ticker}
-          />
-        </>
-      )}
+
+      <PartedMars
+        allTokens={allMintedTokens}
+        myTokens={tokens}
+        height="100vh"
+        handleClaim={claimToken}
+        balance={userBalance}
+        currency={CURRENT_CHAIN.ticker}
+      />
     </div>
   );
 }
